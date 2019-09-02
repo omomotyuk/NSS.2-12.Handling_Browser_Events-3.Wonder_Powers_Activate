aa396b84575f0bf2ab21e9f18cb42181f52e94f9
Alex <o.a.momotyuk@gmail.com>
2019-09-02 15:44:12 -0500

    mindreading and xray part - done

--- src/main.js
+++ src/main.js
@@ -2,19 +2,26 @@
 //
 */
 
-/*
-Write a function named flightHandlerFunction that will remove the disabled class on the corresponding <section id="flight"> (section, not button) and replace it with a class of enabled.
-*/
 //
-function flightHandlerFunction (event) {
-    const sectionElement = document.querySelector('#flight');
-
-    if ( sectionElement.className === "power disabled" ) {
-        sectionElement.className = "power enabled";
-    } else {
-        sectionElement.className = "power disabled";
-    }
+function buttonHandler ( button_id, section_id ) {
+    this.button_id = button_id;
+    this.section_id = section_id;
 }
 
 //
-document.querySelector("#activate-flight").addEventListener( "click", flightHandlerFunction )
+const buttonArray = [];
+buttonArray[0] = new buttonHandler( 'activate-flight', 'flight' );
+buttonArray[1] = new buttonHandler( 'activate-mindreading', 'mindreading' );
+buttonArray[2] = new buttonHandler( 'activate-xray', 'xray' );
+
+//
+buttonArray.forEach( button => {
+    document.querySelector(`#${button.button_id}`).addEventListener( "click", function( event ) {
+        const sectionElement = document.querySelector(`#${button.section_id}`);
+        if ( sectionElement.className === "power disabled" ) {
+            sectionElement.className = "power enabled";
+        } else {
+            sectionElement.className = "power disabled";
+        }
+    })
+});
